dbbd932da736a34d612da1f02044e4abfed8cffa
MikiWojak <mikizarowa8@gmail.com>
2025-02-13 18:24:20 +0100

    Update DatePicker, remove package

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -4,13 +4,17 @@ import { createVuetify } from 'vuetify';
 import '@mdi/font/css/materialdesignicons.css';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import { VDateInput } from 'vuetify/labs/VDateInput';
 
 import App from '@/App.vue';
 import router from '@/router';
 import { store } from '@/store';
 
 const vuetify = createVuetify({
-    components,
+    components: {
+        ...components,
+        VDateInput
+    },
     directives
 });
 
