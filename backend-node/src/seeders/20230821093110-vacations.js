f74473a4edbd7a0194e545c65b7113f9d42550ed
MikiWojak <mikizarowa8@gmail.com>
2025-07-10 20:54:53 +0200

    Refine seeders

--- backend-node/src/seeders/20230821093110-vacations.js
+++ backend-node/src/seeders/20230821093110-vacations.js
@@ -7,6 +7,7 @@ const {
 } = require('../models');
 
 const di = require('../di');
+const faker = require('faker');
 const userRepository = di.get('repositories.user');
 const vacationRepository = di.get('repositories.vacation');
 
@@ -34,10 +35,37 @@ module.exports = {
 
             const [contract] = contracts;
 
-            const startDate = contract.startDate;
-            const endDate = dayjs(contract.startDate)
-                .add(Math.random() * (vacationDaysSum + 10 - 3) + 3, 'day')
-                .format('YYYY-MM-DD');
+            const startDateRaw = faker.date.between(
+                contract.startDate,
+                dayjs(contract.endDate).subtract(60, 'day').format('YYYY-MM-DD')
+            );
+
+            let startDateDayjs = dayjs(startDateRaw);
+
+            if (startDateDayjs.day() === 0 || startDateDayjs.day() === 6) {
+                startDateDayjs = startDateDayjs.add(
+                    startDateDayjs.day() === 6 ? 2 : 1,
+                    'day'
+                );
+            }
+
+            const startDate = dayjs(startDateDayjs).format('YYYY-MM-DD');
+
+            const endDateRaw = faker.date.between(
+                startDate,
+                startDateDayjs.add(20, 'day').format('YYYY-MM-DD')
+            );
+
+            let endDateDayjs = dayjs(endDateRaw);
+
+            if (endDateDayjs.day() === 0 || endDateDayjs.day() === 6) {
+                endDateDayjs = endDateDayjs.add(
+                    endDateDayjs.day() === 6 ? 2 : 1,
+                    'day'
+                );
+            }
+
+            const endDate = dayjs(endDateDayjs).format('YYYY-MM-DD');
 
             const transaction = await vacationRepository.getDbTransaction();
 
