fe8563b696370dfd250a86b3998c57874c0fccdf
MikiWojak <mikizarowa8@gmail.com>
2025-07-13 16:26:03 +0200

    Comment

--- backend-node/src/helpers/handlebars.js
+++ backend-node/src/helpers/handlebars.js
@@ -1,6 +1,7 @@
 const Handlebars = require('handlebars');
 
 module.exports = {
+    // https://handlebarsjs.com/examples/helper-multiple-parameters.html
     link: (text, url) => {
         const urlProcessed = Handlebars.escapeExpression(url);
         const textProcessed = Handlebars.escapeExpression(text);
